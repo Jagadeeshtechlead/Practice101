dadbe08873f510b00e7d61469eaa782ee58451b9
jagadeeshtechlead <jagadeesh.techlead@gmail.com>
2024-07-10 05:55:18 +0530

    Feaature201 changes in application.java file by Vivek

---
+++ application.java
@@ -0,0 +1,38 @@
+// File: MyClass.java
+
+// A class with public access specifier
+public class MyClass {
+    
+    // Public field
+    public int publicField = 10;
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+#Feature201 changes added by Vinay for the payment gateway in Jul2024
+
+// A class with protected access specifier
+public class Animal {
+    
+    // Protected field
+    protected String species = "Unknown"; // Initialize with a default value
+
+    // Protected method
+    protected void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+#End of Feature201 code
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an object of MyClass
+        MyClass myObject = new MyClass();
+
+        // Accessing the public field
+        System.out.println("Public Field: " + myObject.publicField);
+
+        // Calling the public method
+        myObject.publicMethod();
+    }
+}
