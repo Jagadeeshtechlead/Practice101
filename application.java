5b26a5d41dd6d709d0bb5ef2c15bd5fc8ba0bd60
jagadeeshtechlead <jagadeesh.techlead@gmail.com>
2024-07-10 06:22:04 +0530

    Feature202 changes by Vivek

---
+++ application.java
@@ -0,0 +1,36 @@
+// File: MyClass.java
+
+// A class with public access specifier
+public class MyClass {
+    
+    // Public field
+    public int publicField = 10;
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+#Feature202 changes added by Vivek for the payment gateway in Jul2024
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating an object of Dog
+        Dog myDog = new Dog();
+
+        // Accessing protected members through a public method
+        myDog.displayInfo();
+    }
+}
+#End of Feature202 code
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an object of MyClass
+        MyClass myObject = new MyClass();
+
+        // Accessing the public field
+        System.out.println("Public Field: " + myObject.publicField);
+
+        // Calling the public method
+        myObject.publicMethod();
+    }
+}
